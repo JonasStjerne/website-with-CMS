ccfd0d9e1f0c8609bfa033b74b49c7dad8a2459b
Jonas Stjerne <73853586+JonasStjerne@users.noreply.github.com>
2022-10-26 13:35:08 +0200

    About page fetch and display content from api

--- frontend/src/lib/About.svelte
+++ frontend/src/lib/About.svelte
@@ -1,1 +1,19 @@
-<div />
+<script lang="ts">
+  import apiService from "src/api_service";
+  const contentPromise = apiService.getAboutContent();
+</script>
+
+{#await contentPromise}
+  <p>Loading...</p>
+{:then content}
+  <div>
+    <img
+      src={content.data.attributes.picture.data.attributes.url}
+      alt={content.data.attributes.picture.data.attributes.alternativeText}
+    />
+    <div>
+      <h1>{content.data.attributes.heading}</h1>
+      <p>{content.data.attrubutes.content}</p>
+    </div>
+  </div>
+{/await}
