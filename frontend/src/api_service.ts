48fc4dc62247de68cc9b2b1bab66815e6c1c0e8e
Jonas Stjerne <73853586+JonasStjerne@users.noreply.github.com>
2022-10-26 13:49:11 +0200

    Get single painting info

--- frontend/src/api_service.ts
+++ frontend/src/api_service.ts
@@ -1,5 +1,14 @@
 import env from "../env";
 export default class apiService {
+  static async getSinglePainting(id: number) {
+    const reposneJson = await fetch(
+      env.API_BASE_URL + "paintings/" + id + "?populate=*"
+    );
+    const response = await reposneJson.json();
+
+    return response;
+  }
+
   static async getAllPaintings() {
     const responseJSON = await fetch(env.API_BASE_URL + "paintings?populate=*");
     const response = await responseJSON.json();
