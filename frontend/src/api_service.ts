413f8239626d473e69be1e09466d551ade599308
Jonas Stjerne <73853586+JonasStjerne@users.noreply.github.com>
2022-10-26 13:20:57 +0200

    Update api service

--- frontend/src/api_service.ts
+++ frontend/src/api_service.ts
@@ -1,11 +1,15 @@
 import env from "../env";
-export class paintingService {
-  API_TOKEN = env.API_TOKEN;
-  BASE_URL = env.API_BASE_URL;
-
-  async getAllPaintings() {
-    const responseJSON = await fetch(this.BASE_URL + "paintings");
+export default class apiService {
+  static async getAllPaintings() {
+    const responseJSON = await fetch(env.API_BASE_URL + "paintings");
     const response = await responseJSON.json();
+
+    return response;
+  }
+
+  static async getAboutContent() {
+    const reposneJson = await fetch(env.API_BASE_URL + "about");
+    const response = await reposneJson.json();
 
     return response;
   }
