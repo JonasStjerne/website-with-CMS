21c4d9a7a867e629f596be0cee7d672720ef7f41
Jonas Stjerne <73853586+JonasStjerne@users.noreply.github.com>
2022-10-26 13:02:09 +0200

    Fecth css from api

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,6 +1,22 @@
 <script lang="ts">
+  import env from "env";
   import Header from "./lib/Header.svelte";
   import PaintingGrid from "./lib/Painting-grid.svelte";
+
+  //Fetch styles from backend and import them as css variables
+  async function setStylesFromApi() {
+    //Fetch data
+    const responseJSON = await fetch(env.API_BASE_URL + "style");
+    const response = await responseJSON.json();
+
+    //Set css variables with object name and value
+    Object.keys(response.data).forEach((styleName) => {
+      document.documentElement.style.setProperty(
+        `--${styleName}`,
+        response.data[styleName]
+      );
+    });
+  }
 </script>
 
 <Header />
