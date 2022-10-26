d0d123c8133aab48d5eeb8bc361ff50faa8c549b
Jonas Stjerne <73853586+JonasStjerne@users.noreply.github.com>
2022-10-26 13:30:51 +0200

    Get image relation on fetch aswell

--- frontend/src/api_service.ts
+++ frontend/src/api_service.ts
@@ -1,14 +1,14 @@
 import env from "../env";
 export default class apiService {
   static async getAllPaintings() {
-    const responseJSON = await fetch(env.API_BASE_URL + "paintings");
+    const responseJSON = await fetch(env.API_BASE_URL + "paintings?populate=*");
     const response = await responseJSON.json();
 
     return response;
   }
 
   static async getAboutContent() {
-    const reposneJson = await fetch(env.API_BASE_URL + "about");
+    const reposneJson = await fetch(env.API_BASE_URL + "about?populate=*");
     const response = await reposneJson.json();
 
     return response;
