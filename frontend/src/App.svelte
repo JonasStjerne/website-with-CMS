19933dd463fa185083bf2c54290465adcd3b55d5
Jonas Stjerne <j.stjerne@live.dk>
2022-10-27 07:35:35 +0200

    Fix css api fetch

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,19 +1,20 @@
 <script lang="ts">
-  import env from "env";
   import Header from "./lib/Header.svelte";
   import PaintingGrid from "./lib/Painting-grid.svelte";
   import apiService from "./api_service";
+
   setStylesFromApi();
+
   //Fetch styles from backend and import them as css variables
   async function setStylesFromApi() {
     //Fetch data
     const response = await apiService.getStyles();
 
     //Set css variables with object name and value
-    Object.keys(response.data).forEach((styleName) => {
+    Object.keys(response.data.attributes).forEach((styleName) => {
       document.documentElement.style.setProperty(
         `--${styleName}`,
-        response.data[styleName]
+        response.data.attributes[styleName]
       );
     });
   }
